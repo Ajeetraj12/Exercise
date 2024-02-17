994549742c08a89cd554ddb76cef99c617e1c265
Ajeet raj <109308346+Ajeetraj12@users.noreply.github.com>
2024-02-17 07:20:34 +0530

    Enter 3 numbers from the user & make a function to print their average.
    Enter 3 numbers from the user & make a function to print their average.

---
+++ Avarage.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Function {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double avarage = (a+b+c)/3;
+        System.out.println(avarage);
+    }
+}
+
+// Enter 3 numbers from the user & make a function to print their average.
